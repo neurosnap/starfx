f49251a9f288aba7c14142b35684c6ccf03afbc3
Eric Bower <me@erock.io>
2023-04-15 11:24:27 -0400

    explicit exports

--- deps.ts
+++ deps.ts
@@ -1,3 +1,21 @@
 import React from "https://esm.sh/react@18.2.0";
 export { React };
-export * from "https://deno.land/x/effection@3.0.0-alpha.6/mod.ts";
+export type {
+  Channel,
+  Operation,
+  Scope,
+  Task,
+} from "https://deno.land/x/effection@3.0.0-alpha.7/mod.ts";
+export {
+  action,
+  createChannel,
+  createContext,
+  createScope,
+  expect,
+  getframe,
+  resource,
+  run,
+  spawn,
+  useAbortSignal,
+  sleep,
+} from "https://deno.land/x/effection@3.0.0-alpha.7/mod.ts";
