e18a41f321a68d16574fca1d3ec6e9d301cdec16
Eric Bower <me@erock.io>
2023-09-15 22:29:18 -0400

    chore: export supervisors

--- redux/mod.ts
+++ redux/mod.ts
@@ -4,3 +4,4 @@ export * from "./middleware.ts";
 export * from "./types.ts";
 export type { ActionWPayload, AnyAction, AnyState } from "../types.ts";
 export { createSelector } from "../deps.ts";
+export * from "./supervisor.ts";
