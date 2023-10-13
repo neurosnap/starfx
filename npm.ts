307ecf0b8eaa5e0c5b0721afffa1908d572a8173
Eric Bower <me@erock.io>
2023-10-13 10:06:13 -0400

    chore: update `effection` to `beta.0`

--- npm.ts
+++ npm.ts
@@ -30,9 +30,9 @@ async function main() {
       },
     ],
     mappings: {
-      "https://deno.land/x/effection@3.0.0-alpha.15/mod.ts": {
+      "https://deno.land/x/effection@3.0.0-beta.0/mod.ts": {
         name: "effection",
-        version: "3.0.0-alpha.15",
+        version: "3.0.0-beta.0",
       },
       "https://esm.sh/react@18.2.0?pin=v122": {
         name: "react",
