00d7ee5bf5c6a90c058ad73ea29a56e40b2e74a0
Jacob Bolda <me@jacobbolda.com>
2024-01-23 16:45:41 -0600

    keep node_modules for parcel

--- .scripts/sync.ts
+++ .scripts/sync.ts
@@ -18,10 +18,9 @@ await main(function* (): Operation<void> {
     yield* call(command.output());
   } else {
     try {
-      yield* call(Deno.remove("./npm/node_modules", { recursive: true }));
       yield* call(Deno.remove(dir, { recursive: true }));
     } catch (error) {
-      // assume that it doesn't exist
+      // assume that the folder does not exist
     }
 
     // create a symlink to the `dir` which should allow
