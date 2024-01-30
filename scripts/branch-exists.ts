54f6fc8804325494462047e3394e9dc9cdbae945
Jacob Bolda <me@jacobbolda.com>
2024-01-29 22:05:41 -0600

    ecosystem CI tests (#36)
    * initial CI setup
    
    * fix CI workflow name
    
    * consistent CI workflow name with file name
    
    * script to symlink and CI workflow update to run build
    
    * fix folder name in example dir
    
    * try --refresh-lockfile
    
    * try with npm
    
    * try with --no-lockfile
    
    * try with npm including build
    
    * npm run build
    
    * run as matrix
    
    * don't fail fast
    
    * skip remove of ./npm/node_modules
    
    * do file: install for parcel instead
    
    * keep node_modules for parcel
    
    * matrix out path chunks
    
    * lint fix
    
    * another lint fix
    
    * fix CI name
    
    * install both examples
    
    * dynamically pull matching branch
    
    * nested folder
    
    * quiet script
    
    * throw on failed reponse
    
    * pass branch
    
    * more error context
    
    * bad boop
    
    * name branch step
    
    * lint
    
    * build and test examples
    
    * remove GITHUB_TOKEN
    
    * directly append in script
    
    * add logs
    
    * dump output
    
    * dump output file
    
    * dump step
    
    * .outputs
    
    * lint
    
    * really need to fix my local linter
    
    * add a bit to the readme
    
    * rename .scripts/ to scripts/
    
    * update script references

---
+++ scripts/branch-exists.ts
@@ -0,0 +1,52 @@
+import { call, main, type Operation } from "./deps.ts";
+
+await main(function* (): Operation<void> {
+  // based on env created from ${{ secrets.GITHUB_TOKEN }} in CI
+  const token = Deno.env.get("GITHUB_TOKEN");
+  const [branch, ownerRepo] = Deno.args;
+  console.dir({ branch, ownerRepo });
+
+  const response = yield* call(
+    fetch(`https://api.github.com/repos/${ownerRepo}/branches`, {
+      headers: {
+        Accept: "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+        // the token isn't required but helps with rate limiting
+        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      },
+    }),
+  );
+
+  if (response.ok) {
+    const branches = yield* call(response.json());
+    const branchList = branches.map((branch: { name: string }) => branch.name);
+    // for CI debug purposes
+    console.dir({ branchList });
+    // GitHub Actions maintains the step output through a file which you append keys into
+    //   the path that file is available as an env var
+    if (Deno.env.get("CI")) {
+      const output = Deno.env.get("GITHUB_OUTPUT");
+      if (!output) throw new Error("$GITHUB_OUTPUT is not set");
+      const encoder = new TextEncoder();
+      if (branchList.includes(branch)) {
+        const data = encoder.encode(`branch=${branch}`);
+        yield* call(Deno.writeFile(output, data, { append: true }));
+      } else {
+        const data = encoder.encode("branch=main");
+        yield* call(Deno.writeFile(output, data, { append: true }));
+      }
+    }
+    // always log out the branch for both CI and local running
+    if (branchList.includes(branch)) {
+      console.log(`branch=${branch}`);
+    } else {
+      console.log(`branch=main`);
+    }
+  } else {
+    console.error(
+      `Error trying to fetch https://api.github.com/repos/${ownerRepo}/branches and check for ${branch}`,
+    );
+    const text = yield* call(response.text());
+    throw new Error(text);
+  }
+});
