6396f79e423de8a081c0cc247eef50465f933472
Eric Bower <me@erock.io>
2024-02-06 00:08:56 -0500

    fix(ci): shell string

--- test/action.test.ts
+++ test/action.test.ts
@@ -1,14 +1,14 @@
 import { describe, expect, it } from "../test.ts";
-import { API_ACTION_PREFIX, createAction } from "../mod.ts";
+import { createAction } from "../mod.ts";
 
 const tests = describe("createAction()");
 
 it(tests, "should return action type when stringified", () => {
   const undo = createAction("UNDO");
-  expect(`${API_ACTION_PREFIX}UNDO`).toEqual(`${undo}`);
+  expect(`UNDO`).toEqual(`${undo}`);
 });
 
 it(tests, "return object with type", () => {
   const undo = createAction("UNDO");
-  expect(undo()).toEqual({ type: `${API_ACTION_PREFIX}UNDO` });
+  expect(undo()).toEqual({ type: `UNDO` });
 });
