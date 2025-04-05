c5a9e4a1ffbcd27f7044d305173b1711bbe8631f
Jacob Bolda <me@jacobbolda.com>
2025-04-05 00:11:01 -0500

    rewrite react,react-redux as optional peerDeps

--- scripts/npm.ts
+++ scripts/npm.ts
@@ -1,14 +1,30 @@
 import { build, emptyDir } from "jsr:@deno/dnt@0.41.3";
+import path from "node:path";
 
 const [version] = Deno.args;
 if (!version) {
   throw new Error("a version argument is required to build the npm package");
 }
+const outDir = "./npm";
+const mappingPeerDeps = {
+  "react": {
+    ref: "npm:react@^18.2.0",
+    name: "react",
+    version: "^18.2.0",
+    peerDependency: true,
+  },
+  "react-redux": {
+    ref: "npm:react-redux@^8.0.5",
+    name: "react-redux",
+    version: "^8.0.5",
+    peerDependency: true,
+  },
+};
 
 init().then(console.log).catch(console.error);
 
 async function init() {
-  await emptyDir("./npm");
+  await emptyDir(outDir);
   await build({
     declaration: "inline",
     scriptModule: "cjs",
@@ -27,10 +43,21 @@ async function init() {
       "https://deno.land/x/effection@3.0.0-beta.3/mod.ts": {
         name: "effection",
         version: "3.0.0-beta.3",
+        peerDependency: false,
+      },
+      [mappingPeerDeps["react"].ref]: {
+        name: mappingPeerDeps["react"].name,
+        version: mappingPeerDeps["react"].version,
+        peerDependency: mappingPeerDeps["react"].peerDependency,
+      },
+      [mappingPeerDeps["react-redux"].ref]: {
+        name: mappingPeerDeps["react-redux"].name,
+        version: mappingPeerDeps["react-redux"].version,
+        peerDependency: mappingPeerDeps["react-redux"].peerDependency,
       },
     },
     importMap: "deno.json",
-    outDir: "./npm",
+    outDir,
     shims: {
       deno: false,
     },
@@ -68,5 +95,29 @@ async function init() {
       Deno.copyFileSync("LICENSE.md", "npm/LICENSE.md");
       Deno.copyFileSync("README.md", "npm/README.md");
     },
+  });
+
+  // npm mappings aren't supported with peerDeps
+  // https://github.com/denoland/dnt/issues/433
+  const packageJsonPath = path.join(outDir, "package.json");
+  await Deno.readTextFile(packageJsonPath).then(async (v) => {
+    console.log("rewriting some deps to peerDeps");
+    const initial = JSON.parse(v);
+    delete initial.dependencies[mappingPeerDeps["react"].name];
+    delete initial.dependencies[mappingPeerDeps["react-redux"].name];
+    initial.peerDependencies = {
+      [mappingPeerDeps["react"].name]: mappingPeerDeps["react"].version,
+      [mappingPeerDeps["react-redux"].name]:
+        mappingPeerDeps["react-redux"].version,
+    };
+    initial.peerDependenciesMeta = {
+      [mappingPeerDeps["react"].name]: {
+        "optional": true,
+      },
+      [mappingPeerDeps["react-redux"].name]: {
+        "optional": true,
+      },
+    };
+    await Deno.writeTextFile(packageJsonPath, JSON.stringify(initial, null, 2));
   });
 }
