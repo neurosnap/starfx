9bafac3894d9e30809f8c2734e5353c95acd2581
Jacob Bolda <me@jacobbolda.com>
2025-07-06 21:59:47 -0500

    use root react due to symlink and jest weirdness

--- examples/tests-rtl/jest.config.js
+++ examples/tests-rtl/jest.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+export default {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.(t|j)sx?$": "babel-jest",
   },
   setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  moduleNameMapper: {
+    "^react$": "<rootDir>/node_modules/react",
+    "^react-dom$": "<rootDir>/node_modules/react-dom",
+  },
 };
